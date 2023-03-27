647764080b1fe17d5e04106b9aef4eb6d799ef78
Dirga Yasa <cekingx@gmail.com>
2023-03-27 21:08:19 +0800

    feat: some update

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ pub mod palindrom_number;
 use palindrom_number::palindrom_number::Solution;
 
 fn main() {
-    println!("is 121 palindrom: {}", Solution::is_palindrome(121));
+    println!("is 121 palindrome: {}", Solution::is_palindrome(121));
     println!("is 1221 palindrom: {}", Solution::is_palindrome(1221));
     println!("is 123 palindrom: {}", Solution::is_palindrome(123));
 }
