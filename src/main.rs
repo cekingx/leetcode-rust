9863077194ca8e9f745de357639fbafb189d5a95
Dirga Yasa <cekingx@gmail.com>
2023-05-01 17:31:16 +0800

    merge_two_sorted_lists: work

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
-mod valid_parentheses;
-use valid_parentheses::self_solution::Solution;
+mod merge_two_sorted_lists;
+use merge_two_sorted_lists::self_solution::{Solution, ListNode};
 
 fn main() {
-    println!("{}", Solution::is_valid(String::from("([)]")));
+    let left = Some(Box::new(ListNode {
+        val: 1,
+        next: Some(Box::new(ListNode {
+            val: 2,
+            next: None
+        }))
+    }));
+    let right = Some(Box::new(ListNode::new(2)));
+    println!("{:?}", Solution::merge_two_lists(left, right));
 }
